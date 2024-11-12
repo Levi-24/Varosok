8c3b4402059798ea2100b6a6afe4fef49ebf4a0e
Levente <122294490+Levi-24@users.noreply.github.com>
2024-11-12 13:54:11 +0100

    Update

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿//Major Levente 2024.11.12.
-using Cities;
+﻿using Cities;
 
 List<City> cities = new();
 using StreamReader reader = new($"../../../src/varosok.csv");
@@ -19,12 +18,10 @@ Console.WriteLine("3. Feladat:");
 Console.WriteLine("\t" + cities.OrderBy(c =>c.Population).Last());
 
 Console.WriteLine("4. Feladat:");
-var overTwentyMill = cities.Where(c => c.Population > 20).OrderByDescending(c => c.Population).ToList();
-foreach (var city in overTwentyMill) Console.WriteLine("\t" + city);
+cities.Where(c => c.Population > 20).OrderByDescending(c => c.Population).ToList().ForEach(c => { Console.WriteLine("\t" + c); });
 
 Console.WriteLine("5. Feladat:");
-var multipleCities = cities.GroupBy(c => c.CountryName).Where(g => g.Count() > 1).ToList();
-Console.WriteLine("\t" + multipleCities.Count);
+Console.WriteLine("\t" + cities.GroupBy(c => c.CountryName).Where(g => g.Count() > 1).Count());
 
 Console.WriteLine("6. Feladat:");
 Console.WriteLine("\t" + cities.GroupBy(c => c.CityName[0]).OrderByDescending(g => g.Count()).First().Key);
